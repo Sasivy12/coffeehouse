42a063a36df19486ac39562e31d224f1339987b8
Sasivy12 <muslimov.tim05@gmail.com>
2024-05-26 22:07:36 +0300

    all coffeehouse methods are implemented

--- src/main/java/com/example/coffeehouse/CoffeeHouse/CoffeeHouseController.java
+++ src/main/java/com/example/coffeehouse/CoffeeHouse/CoffeeHouseController.java
@@ -1,11 +1,10 @@
 package com.example.coffeehouse.CoffeeHouse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +17,29 @@ public class CoffeeHouseController
     public List<CoffeeHouse> getAllCoffeeHouses()
     {
         return coffeeHouseService.getAllCoffeeHouses();
+    }
+
+    @PostMapping("/add_coffee_house")
+    public CoffeeHouse createCoffeeHouse(@RequestBody CoffeeHouse coffeeHouse)
+    {
+        return coffeeHouseService.createCoffeeHouse(coffeeHouse);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<CoffeeHouse> getASpecificCoffeeHouse(@PathVariable Long id)
+    {
+        return coffeeHouseService.getASpecificCoffeeHouse(id);
+    }
+
+    @PutMapping("/{id}/edit")
+    public CoffeeHouse updateTheCoffeeHouse(@PathVariable Long id, @RequestBody CoffeeHouse updatedCoffeeHouse)
+    {
+        return coffeeHouseService.updateTheCoffeeHouse(id, updatedCoffeeHouse);
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<CoffeeHouse> deleteTheCoffeeHouse(@PathVariable Long id)
+    {
+        return coffeeHouseService.deleteTheCoffeeHouse(id);
     }
 }
--- src/main/java/com/example/coffeehouse/CoffeeHouse/CoffeeHouseService.java
+++ src/main/java/com/example/coffeehouse/CoffeeHouse/CoffeeHouseService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -15,5 +16,50 @@ public class CoffeeHouseService
     public List<CoffeeHouse> getAllCoffeeHouses()
     {
         return coffeeHouseRepository.findAll();
+    }
+
+    public CoffeeHouse createCoffeeHouse(CoffeeHouse coffeeHouse)
+    {
+        return coffeeHouseRepository.save(coffeeHouse);
+    }
+
+    public Optional<CoffeeHouse> getASpecificCoffeeHouse(Long id)
+    {
+        return coffeeHouseRepository.findById(id);
+    }
+
+    public CoffeeHouse updateTheCoffeeHouse(Long id, CoffeeHouse updatedCoffeeHouse)
+    {
+        Optional<CoffeeHouse> existingCoffeeHouse = coffeeHouseRepository.findById(id).stream().findFirst();
+
+        if(existingCoffeeHouse.isPresent())
+        {
+            CoffeeHouse coffeeHouse = existingCoffeeHouse.get();
+
+            coffeeHouse.setName(updatedCoffeeHouse.getName());
+            coffeeHouse.setDateCreated(updatedCoffeeHouse.getDateCreated());
+            coffeeHouse.setSchedule(updatedCoffeeHouse.getSchedule());
+            coffeeHouse.setInfo(updatedCoffeeHouse.getInfo());
+
+            coffeeHouseRepository.save(coffeeHouse);
+        }
+        else
+        {
+            throw new RuntimeException("CoffeeHouse with id " + id + " not found");
+        }
+
+        return updatedCoffeeHouse;
+    }
+
+    public Optional<CoffeeHouse> deleteTheCoffeeHouse(Long id)
+    {
+        Optional<CoffeeHouse> coffeeHouse = coffeeHouseRepository.findById(id);
+
+        if(coffeeHouse.isPresent())
+        {
+            coffeeHouseRepository.deleteById(id);
+        }
+
+        return coffeeHouse;
     }
 }
